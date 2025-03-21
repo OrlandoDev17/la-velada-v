af2297488565e83e2749fb36cd2f69677b3f7b3e
OrlandoDev17 <orlandodev1707@gmail.com>
2025-03-21 00:56:43 -0400

    Agregar información adicional de los peleadores: edad, peso y altura en el componente FightersCard

--- src/components/FighterCard.jsx
+++ src/components/FighterCard.jsx
@@ -4,58 +4,100 @@ const FIGHTERS = [
   {
     name: 'TheGrefg',
     flag: 'spain',
+    age: '27',
+    weight: '70.9',
+    height: '1.78',
   },
   {
     name: 'WestCol',
     flag: 'colombia',
+    age: '24',
+    weight: '66',
+    height: '1.65',
   },
   {
     name: 'Viruzz',
     flag: 'spain',
+    age: '32',
+    weight: '78.5',
+    height: '1.82',
   },
   {
     name: 'Mazza',
     flag: 'argentina',
+    age: '24',
+    weight: '84',
+    height: '1.76',
   },
   {
     name: 'Alana',
     flag: 'mexico',
+    age: '24',
+    weight: '56',
+    height: '1.56',
   },
   {
     name: 'Arigeli',
     flag: 'spain',
+    age: '22',
+    weight: '56',
+    height: '1.60',
   },
   {
     name: 'Andonii',
     flag: 'spain',
+    age: '22',
+    weight: '115',
+    height: '1.87',
   },
   {
     name: 'Belcast',
     flag: 'mexico',
+    age: '26',
+    weight: '90',
+    height: '1.78',
   },
   {
     name: 'Abby',
     flag: 'spain',
+    age: '24',
+    weight: '57',
+    height: '1.72',
   },
   {
     name: 'RoRo',
     flag: 'spain',
+    age: '23',
+    weight: '57',
+    height: '1.49',
   },
   {
     name: 'Perxita',
     flag: 'spain',
+    age: '33',
+    weight: '80',
+    height: '1.86',
   },
   {
     name: 'Gaspi',
     flag: 'argentina',
+    age: '22',
+    weight: '85',
+    height: '1.75',
   },
   {
     name: 'Pereira',
     flag: 'spain',
+    age: '26',
+    weight: '63',
+    height: '1.67',
   },
   {
     name: 'Rivaldios',
     flag: 'mexico',
+    age: '24',
+    weight: '67',
+    height: '1.70',
   },
 ];
 
@@ -66,7 +108,7 @@ const FightersCard = () => {
   return (
     <div className="relative flex flex-col items-center">
       {/* Imagen Ampliada */}
-      <picture className="relative flex flex-col justify-center items-center mb-8">
+      <article className="relative flex flex-col justify-center items-center mb-8">
         <img
           className="absolute top-28"
           src="./images/logo-la-velada-v.webp"
@@ -87,17 +129,32 @@ const FightersCard = () => {
         <h2 className="text-7xl uppercase text-primary text-center mt-2 absolute -bottom-8">
           {hoveredFighter.name}
         </h2>
-      </picture>
+        <p className="absolute -left-48 text-5xl uppercase font-bold text-primary">
+          {hoveredFighter.age} Años
+        </p>
+        <div className="absolute -right-46">
+          <p className="text-5xl uppercase font-bold text-primary mb-4">
+            {hoveredFighter.weight} KG
+          </p>
+          <p className="text-5xl uppercase font-bold text-primary">
+            {hoveredFighter.height} M
+          </p>
+        </div>
+      </article>
 
       {/* Contenedor de las Cards */}
       <div className="relative flex flex-wrap justify-center gap-x-5 gap-y-16 max-w-236 mt-24">
         {/* Renderizado de las Cards */}
-        {FIGHTERS.map(({ name, flag }) => (
+        {FIGHTERS.map(({ name, flag, age, weight, height }) => (
           <article
             key={name}
             className="bg-gradient-to-b from-transparent to-primary h-18 flex items-end relative hover:scale-125 transition cursor-pointer"
-            onMouseEnter={() => setHoveredFighter({ name, flag })}
-            onMouseLeave={() => setHoveredFighter({ name, flag })}
+            onMouseEnter={() =>
+              setHoveredFighter({ name, flag, age, weight, height })
+            }
+            onMouseLeave={() =>
+              setHoveredFighter({ name, flag, age, weight, height })
+            }
           >
             <img
               className="w-28 relative"
